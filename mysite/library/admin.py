982c8de30ec6b0f76eb7b4a2c29adfbb1364ae05
Donatas Noreika <donoras@gmail.com>
2020-05-19 10:18:22 +0300

    2 (3) paskaita - administratoriaus puslapio sutvarkymas

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -3,7 +3,38 @@ from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
 
-admin.site.register(Author)
+class BooksInline(admin.TabularInline):
+    model = Book
+    readonly_fields = ('title', 'author', 'summary', 'isbn', 'genre')
+    can_delete = False
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
+    inlines = [BooksInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    search_fields = ('id', 'book__title')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
